3d6b50b22ff5aa0872c1f1b6419e15918c587b5d
Hurray Banana <HurrayBanana@users.noreply.github.com>
2020-11-13 10:54:04 +0000

    Latest version of stack code

--- DataStructures/Stack.cs
+++ DataStructures/Stack.cs
@@ -4,10 +4,22 @@ using System.Text;
 
 namespace DataStructures
 {
+    /// <summary>
+    /// implements an integer based stack
+    /// </summary>
     class Stack
     {
+        /// <summary>
+        /// stack space
+        /// </summary>
         int[] stack;
+        /// <summary>
+        /// stack pointer
+        /// </summary>
         int sp;
+        /// <summary>
+        /// size set at construction
+        /// </summary>
         int max;
 
         public Stack(int size)
@@ -17,22 +29,30 @@ namespace DataStructures
             sp = 0;
         }
 
+        /// <summary>
+        /// places an item if possible onto top of stack
+        /// </summary>
+        /// <param name="item"></param>
         public void Push(int item)
         {
             if (sp < max)
             {
-                stack[sp++] = item;
-                //sp++;
+                stack[sp] = item;
+                sp++;
             }
             throw new Exception("STACK FULL");
         }
 
+        /// <summary>
+        /// removes an item from the top of stack if possible
+        /// </summary>
+        /// <returns></returns>
         public int Pop()
         {
             if (sp > 0)
             {
-                //sp--;
-                return stack[--sp];
+                sp--;
+                return stack[sp];
             }
             throw new Exception("STACK EMPTY");
         }
